4ec939ebca7153e4acf8c17c6df7bcf375cafdf8
Nick Matvyeyev <mnasoft@gmail.com>
2019-08-29 14:03:15 +0300

    2019-8-29 14:3:8 MNASOFT-01

--- package.lisp
+++ package.lisp
@@ -3,3 +3,5 @@
 (defpackage #:algorithm
   (:use #:cl #:mnas-hash-table #:mnas-graph)
   (:export make-create))
+
+;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
